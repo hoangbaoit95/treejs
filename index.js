7da7f2a523818bb2c45154478be33e07cc2c8568
DESKTOP-LAEEAL6\Pham Hoang Bao <hoangbaoit95@gmail.com>
2018-06-06 19:53:51 +0700

    index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD!')
+  res.send('HELLO WORLD!');
 })
-app.listen(80, () => console.log('Server running on port 3000'))
+app.listen(80, () => console.log('Server running on port 3000'));
